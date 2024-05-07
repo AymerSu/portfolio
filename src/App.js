e8a5bbcf0ca5d9a2e80f2873cbafcd43ec31e5a9
AymerSu <aymeryesid.aycs@gmail.com>
2024-05-06 19:00:20 -0500

    Create first component description

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Text } from './Text';
+
+const texts = [
+  {text: "Bienvenido a mi sitio web personal, donde puedes explorar una selección curada de mis proyectos, profundizar en mis habilidades y leer detenidamente mi currículum. Como desarrollador experimentado, he tenido el privilegio de trabajar en una amplia gama de proyectos, cada uno de los cuales muestra mi experiencia y pasión por crear soluciones innovadoras. Ya sea que esté interesado en desarrollo web, aplicaciones móviles o ingeniería de software, aquí encontrará un vistazo de mis capacidades y experiencias. No dude en comunicarse si tiene alguna pregunta u oportunidad de colaborar."}
+];
+
+const description = 'Bienvenido a mi sitio web personal, donde puedes explorar una selección curada de mis proyectos, profundizar en mis habilidades y leer detenidamente mi currículum. Como desarrollador experimentado, he tenido el privilegio de trabajar en una amplia gama de proyectos, cada uno de los cuales muestra mi experiencia y pasión por crear soluciones innovadoras. Ya sea que esté interesado en desarrollo web, aplicaciones móviles o ingeniería de software, aquí encontrará un vistazo de mis capacidades y experiencias. No dude en comunicarse si tiene alguna pregunta u oportunidad de colaborar.';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+       <Text text={description}/>
+    </React.Fragment>
   );
 }
 
