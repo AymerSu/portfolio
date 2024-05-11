84269126282fa5b0c18f805154b0a757f6335e16
AymerSu <aymeryesid.aycs@gmail.com>
2024-05-10 23:47:23 -0500

    Add necessary components and structure home page

--- src/components/Tarjet.js
+++ src/components/Tarjet.js
@@ -1,11 +1,37 @@
 import './../styles/Tarjet.css';
+import { Buttons } from './Buttons';
+
+const textButtons = "Detallar"; 
 
 function Tarjet() {
     return (
+    <div class="container-tarjet">
         <div class="tarjeta">
-            <h2>Tarjeta 1</h2>
-            <p>Contenido de la tarjeta 1.</p>
+            <h1>Experiencia</h1>
+            <h3>Banco Davivienda</h3>
+            <p>Desarrollador Junior incorporado al Bootcamp TechCamp del banco Davivienda por una duración de 6 meses</p>
+            <Buttons textoButton={textButtons}></Buttons>
+            <hr></hr>
+            <h3>Ediciones Gamma S.A.</h3>
+            <p>Analista de sistemas en la coordinación de sistemas, automatizando procesos de diferentes areas por una duración de 6 años.</p>
+            <Buttons textoButton={textButtons}></Buttons>
         </div>
+        
+        <div class="tarjeta">
+            <h1>Estudios</h1>
+            <h3>SENA</h3>
+            <p>Analisis y desarrollo de sistemas de información</p>
+            <Buttons textoButton={textButtons}></Buttons>
+            <hr></hr>
+            <h3>SENA</h3>
+            <p>Técnico de sistemas</p>
+            <Buttons textoButton={textButtons}></Buttons>
+            <hr></hr>
+            <h3>Certificados</h3>
+            <p>Cuento con varios certiciados emitidos por entidades educativas que mejoran mis habilidades.</p>
+            <Buttons textoButton={textButtons}></Buttons>
+        </div>
+    </div>
     );
 }
 
--- src/components/Text.js
+++ src/components/Text.js
@@ -1,6 +1,12 @@
+import { Image } from './Image';
+import './../styles/Text.css';
+
 function Text(props) {
     return (
+    <div class="contenedor-text-img">
         <p>{props.text}</p>
+        <Image></Image>
+    </div>
     )
 }
 
